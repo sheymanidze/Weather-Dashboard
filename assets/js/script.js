c5ba3553665fb3fe7c071e69a77d832e1b18a590
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-03-23 21:52:30 -0400

    api link fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var placesEl = $('#places');
 var searchBtn = $('#searchBtn');
 var clearAll = $('#clearAll');
+var inputCity = $('#city-Input');
+var place = "";
 
 
 
@@ -9,6 +11,11 @@ searchBtn.click(function () {
   //get name and its value
   var cities = $('input[id="city-Input"]').val();
   console.log(cities)
+
+  //refactor url
+  var weatherUrl = generateURL(cities);
+  currentWeather(weatherUrl)
+
 
 
   //if empty
@@ -19,7 +26,7 @@ searchBtn.click(function () {
 
   //print
   placesEl.append('<li>' + cities + '</li>');
-  $(placesEl).attr("class", "list-group-item")
+  $(placesEl).children().attr("class", "list-group-item")
   $('.list-group').css('visibility', 'visible');
   $('.btn-secondary').css('visibility', 'visible');
   console.log($(placesEl))
@@ -38,25 +45,37 @@ clearAll.click(function () {
   location.reload(true);
 });
 
-//API key
-var key = "a51312ec772d50f8e3864848a74cedc0";
-console.log(key);
+function generateURL(cities) {
+  var key = "a51312ec772d50f8e3864848a74cedc0";
+  console.log(key);
 
-var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=&appid={a51312ec772d50f8e3864848a74cedc0}";
-
-console.log(weatherUrl);
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cities + '&appid=' + key;
+  console.log(weatherUrl);
+  return weatherUrl;
+}
 
 function showWeather(event) {
   event.preventDefault();
-}
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=&appid={a51312ec772d50f8e3864848a74cedc0}', {
-  method: 'GET',
-  url: weatherUrl
-})
-  .then(function (response) {
-    return response.json();
+  if (inputCity.val() !== "") {
+    place = inputCity.value();
+    currentWeather(place);
+    //console.log(currentWeather);
+  }
+};
+
+function currentWeather(weatherUrl) {
+
+  fetch(weatherUrl, {
+    // method: 'GET',
+    // url: weatherUrl
   })
-  .then(function (data) {
-    console.log(data);
-  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+    })
+  console.log(currentWeather);
+}
