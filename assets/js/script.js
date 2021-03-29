23dd241df243e3fe1c105256685495a470b13ae6
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-03-29 13:02:49 -0400

    5 days forecast fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,19 @@ var placesEl = $('#places');
 var searchBtn = $('#searchBtn');
 var clearAll = $('#clearAll');
 var inputCity = $('#city-Input');
-//var place = "";
 var cityName = $('#cityName');
 var key = "a51312ec772d50f8e3864848a74cedc0"
 
 
 $('#places').on('click', '.list-group-item', function () {
-  //console.log($(this))
   let clickedCity = ''
   clickedCity = $(this).html();
   console.log(clickedCity)
   var weatherUrl = generateURL(clickedCity);
-  currentWeather(weatherUrl)
-  //console.log(currentWeather)
-  //console.log("Hello")
+  currentWeather(weatherUrl);
+
+  weatherForecast(clickedCity);
+
 })
 
 
@@ -23,7 +22,6 @@ searchBtn.click(function () {
 
   //get name and its value
   var cities = $('input[id="city-Input"]').val();
-  // console.log(cities)
 
   //refactor url
   var weatherUrl = generateURL(cities);
@@ -42,16 +40,13 @@ searchBtn.click(function () {
   $(placesEl).children().attr("class", "list-group-item")
   $('.list-group').css('visibility', 'visible');
   $('.btn-secondary').css('visibility', 'visible');
-  // console.log($(placesEl))
-
-
 
   //clear input
   $('input[id="city-Input"]').val('');
 
   //save at local storage
   localStorage.setItem('places', cities);
-  localStorage.getItem('places', cities);
+  //localStorage.getItem('places', cities);
 
 });
 
@@ -61,23 +56,11 @@ clearAll.click(function () {
 });
 
 function generateURL(cities) {
-  //var key = "a51312ec772d50f8e3864848a74cedc0";
-  // console.log(key);
 
   var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cities + '&appid=' + key;
-  // console.log(weatherUrl);
   return weatherUrl;
 }
 
-// function showWeather(event) {
-//   event.preventDefault();
-
-//   if (inputCity.val() !== "") {
-//     place = inputCity.value();
-//     currentWeather(place);
-//     //console.log(currentWeather);
-//   }
-// };
 
 function currentWeather(weatherUrl) {
 
@@ -87,48 +70,38 @@ function currentWeather(weatherUrl) {
       return response.json();
     })
     .then(function (data) {
-      //console.log(data);
-      // console.log(data.name);
-      // console.log(data.main.temp);
-      // console.log(data.wind.speed);
+
 
 
       var currentDate = moment().format('L');
-      //console.log(currentDate)
-
 
       //Api for icon
       var weatherIcon = data.weather[0].icon;
       var iconUrl = 'https://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
-      // console.log(weatherIcon);
-      // console.log(iconUrl);
 
 
       $('#cityName').text(data.name + " " + "(" + currentDate + ")");
-      $('#iconMain').attr('src', iconUrl)
-      // console.log(cityName);
+      $('#iconMain').attr('src', iconUrl);
+
 
       var temp = (data.main.temp - 273.15);
-      //console.log(temp)
+
       $('#temperature').text('Temperature:  ' + temp.toFixed(0) + ' °C');
-      //console.log(temperature);
+
 
       $('#humidity').text('Humidity:  ' + data.main.humidity + "%");
-      //console.log(humidity);
+
 
       $('#windSpeed').text('Wind Speed:  ' + data.wind.speed.toFixed(1) + ' MPH');
-      //console.log(windSpeed);
 
-      //var key = "a51312ec772d50f8e3864848a74cedc0";
+
       var lat = data.coord.lat;
       var lon = data.coord.lon;
-      // console.log(lat);
-      // console.log(lon);
+
 
 
       //get UV Index
       var uvIndexUrl = 'https://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + key;
-      //console.log(uvIndexUrl);
 
       fetch(uvIndexUrl, {
       })
@@ -136,12 +109,12 @@ function currentWeather(weatherUrl) {
           return response.json();
         })
         .then(function (data) {
-          //console.log(data.value);
+
 
           var coloredIndex = data.value
 
           $('#uvIndex').text('UV Index:  ' + coloredIndex);
-          // console.log(uvIndex)
+
 
           if (data.value <= 2) {
             coloredIndex.css('background-color', 'green')
@@ -163,9 +136,9 @@ function currentWeather(weatherUrl) {
 
 };
 
+//5 days forecast
 function weatherForecast(cities) {
   var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cities + '&units=metric&appid=' + key;
-  console.log(forecastUrl)
 
   fetch(forecastUrl, {
   })
@@ -175,19 +148,13 @@ function weatherForecast(cities) {
     .then(function (data) {
       console.log(data);
       for (i = 0; i < 5; i++) {
-        // var currentDate = moment().format('L');
-        // var fDate = moment(currentDate).add(((i + 1) * 8) - 1).format('L')
-        //console.log(fDate)
+        var date = new Date((data.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
         var forecastIcon = data.list[((i + 1) * 8) - 1].weather[0].icon;
-        console.log(forecastIcon)
         var iconUrl = 'https://openweathermap.org/img/wn/' + forecastIcon + '.png';
-        console.log(iconUrl)
         var temp = data.list[((i + 1) * 8) - 1].main.temp.toFixed(0);
-        console.log(temp)
         var hum = data.list[((i + 1) * 8) - 1].main.humidity;
-        console.log(hum)
 
-        $('#fDate')
+        $('#fDate' + i).text(date);
         $('#forecastIcon' + i).html('<img src=' + iconUrl + '>');
         $('#temp' + i).text(temp + ' °C');
         $('#hum' + i).text(hum + ' %');
